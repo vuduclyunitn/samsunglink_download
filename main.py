f18d501fe7d35882bbf7332f5ca77ace838fc46f
vuduclyunitn <32923740+vuduclyunitn@users.noreply.github.com>
2018-12-05 00:13:25 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+import urllib.request
+import hashlib
+import os
+
+url = "https://linksharing.samsungcloud.com/1543954684866C8ncen6"
+html = urlopen(url)
+soup = bs(html.read())
+
+download_links = {link['longdesc']:link['alt'] for link in soup.find_all('img') if link.has_attr('longdesc')}
+
+output_dir = input("Output dir: ")
+
+for url, file_name in download_links.items():
+    file_path = os.path.join(output_dir, file_name)
+    urllib.request.urlretrieve(url, file_path)
